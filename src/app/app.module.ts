080e92e4ad5f4427ffc46b5c81bee8d525410e95
harishkar <3harish.m@gmail.com>
2019-05-31 00:50:18 +0530

    material module removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,8 @@
-import { MatSelectModule } from '@angular/material/select';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -13,9 +11,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
     AppRoutingModule,
-    MatSelectModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
