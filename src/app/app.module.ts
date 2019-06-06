adfdac16afa34dd540435f24c83d9f8b3e0758ac
Harishkar M <3harish.m@gmail.com>
2019-06-06 09:53:20 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
+import { MatSelectModule } from '@angular/material/select';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -11,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
+    MatSelectModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
